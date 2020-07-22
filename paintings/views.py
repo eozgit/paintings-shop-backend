2238daff2f01365bf316035911c5a0bb2742f483
Enis Ozgit <eozgit@gmail.com>
2020-07-22 18:01:53 +0100

    Add pagination and filtering

--- paintings/views.py
+++ paintings/views.py
@@ -5,8 +5,48 @@ from .serializers import PaintingSerializer
 
 
 class PaintingList(generics.ListCreateAPIView):
-    queryset = Painting.objects.all()
     serializer_class = PaintingSerializer
+
+    def get_queryset(self):
+        queryset = Painting.objects.all()
+
+        title = self.request.query_params.get('title', None)
+        if title is not None:
+            queryset = queryset.filter(title__icontains=title)
+
+        date = self.request.query_params.get('date', None)
+        if date is not None:
+            queryset = queryset.filter(date__year=date)
+
+        media = self.request.query_params.get('media', None)
+        if media is not None:
+            queryset = queryset.filter(media__icontains=media)
+
+        height_min = self.request.query_params.get('height_min', None)
+        if height_min is not None:
+            queryset = queryset.filter(height__gt=height_min)
+
+        height_max = self.request.query_params.get('height_max', None)
+        if height_max is not None:
+            queryset = queryset.filter(height__lt=height_max)
+
+        width_min = self.request.query_params.get('width_min', None)
+        if width_min is not None:
+            queryset = queryset.filter(width__gt=width_min)
+
+        width_max = self.request.query_params.get('width_max', None)
+        if width_max is not None:
+            queryset = queryset.filter(width__lt=width_max)
+
+        price_min = self.request.query_params.get('price_min', None)
+        if price_min is not None:
+            queryset = queryset.filter(price__gt=price_min)
+
+        price_max = self.request.query_params.get('price_max', None)
+        if price_max is not None:
+            queryset = queryset.filter(price__lt=price_max)
+
+        return queryset
 
 
 class PaintingDetail(generics.RetrieveUpdateDestroyAPIView):
--- shop/settings.py
+++ shop/settings.py
@@ -144,6 +144,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4
 }
 
 CORS_ORIGIN_ALLOW_ALL=True
